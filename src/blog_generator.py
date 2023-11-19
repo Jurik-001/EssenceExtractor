b5294535115a0a83c1ef3187ec54c8bdeab1c3ff
Jurik-001 <jurik.eineemail@gmail.com>
2023-11-19 15:58:35 +0100

    add refine feature

--- src/blog_generator.py
+++ src/blog_generator.py
@@ -3,20 +3,28 @@
 import os
 
 import tiktoken
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.messages import HumanMessage, SystemMessage
+from openai import OpenAI
 
 from src import utils
 
-MAX_CONTENT_LENGTH = 4000
+MODEL_TOKEN_LENGTH_MAPPING = {
+    "gpt-3.5-turbo": 4096,
+    "gpt-3.5-turbo-1106": 16385,
+    "gpt-4-1106-preview": 128000,
+    "gpt-4": 8192,
+    "gpt-4-32k": 32768,
+}
+TOKEN_LENGTH_BUFFER = 150
+OUTPUT_TOKEN_LENGTH_BUFFER = 1500
 
 
 class BlogGenerator:
     """Generate a blog post from a text file."""
 
     def __init__(self, output_path="blogs", model_name="gpt-3.5-turbo"):
+        self.model_name = model_name
         self.output_path = output_path
-        self.chat = ChatOpenAI(model_name=model_name)
+        self.client = OpenAI()
         self.encoding = tiktoken.encoding_for_model(model_name)
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
@@ -31,8 +39,7 @@ class BlogGenerator:
         return token_count
 
     def _matches_token_size(self, token_count):
-        # TODO extract supported token length from
-        if token_count > MAX_CONTENT_LENGTH:
+        if token_count > MODEL_TOKEN_LENGTH_MAPPING[self.model_name]:
             return False
         return True
 
@@ -52,9 +59,41 @@ class BlogGenerator:
 
         return formatted_text
 
-    def _entry_generator(self, messages):
-        return self.chat.invoke(messages).content
+    def _generate_answer(self, system_prompt, user_prompt):
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt}
+            ]
+        )
+        return response.choices[0].message.content
 
+    def split_into_first_chunk(self, text, chunk_size, separator=" "):
+        words = text.split(separator)
+        current_chunk = []
+        current_count = 0
+
+        for word in words:
+            current_chunk.append(word)
+            current_count = self._count_tokens(separator.join(current_chunk))
+
+            if current_count >= chunk_size:
+                return separator.join(current_chunk)
+
+        return separator.join(current_chunk)
+
+    def create_refine_prompt(self, existing_answer, text):
+        return (
+            f"We have provided an existing blog article up to a certain point: {existing_answer}\n"
+            "We have the opportunity to refine the existing blog article"
+            "(only if needed) with some more context below.\n"
+            "------------\n"
+            f"{text}\n"
+            "------------\n"
+            "Given the new context, refine the original blog article"
+            "If the context isn't useful, return the original blog article."
+        )
 
     def generate_blog(self, text_file_path):
         """Generate a blog post from a text file.
@@ -64,28 +103,35 @@ class BlogGenerator:
 
         Returns:
             str: The path to the generated blog post.
-
-        Raises:
-            ValueError: If the total token count exceeds MAX_CONTENT_LENGTH.
         """
         input_text = self._read_text_file(text_file_path)
-        system_message_content = "You're a tech blog writer. And write blog entries using markedown and placing images using [image] tag."
-        token_count = self._count_tokens(system_message_content) + self._count_tokens(
-            input_text,
-        )
+        system_message = "Your role is creating one final version of a ready to publish article based on transcript. Write the article with a focus on educating the reader and a captivating introduction, body, and a concise conclusion, use markdown and placing images using [image] tag.\n"
+        input_text_length = self._count_tokens(input_text)
+        system_msg_length = self._count_tokens(system_message)
 
-        blog_post = None
-
-        if self._matches_token_size(token_count):
-            messages = [
-                SystemMessage(content=system_message_content),
-                HumanMessage(content=input_text),
-            ]
-            blog_post = self._entry_generator(messages)
+        if self._matches_token_size(system_msg_length + input_text_length + OUTPUT_TOKEN_LENGTH_BUFFER):
+            return self._generate_answer(system_message, input_text)
         else:
-            utils.logging.info(f"The total token count exceeds {MAX_CONTENT_LENGTH}, long format support will be used.")
-            # TODO split up text and generate segments, rewrite sys message so model is aware it generates only segments
+            utils.logging.info(f"The total token count exceeds {MODEL_TOKEN_LENGTH_MAPPING[self.model_name]}, long format support will be used.")
+            user_msg_length = self._count_tokens(self.create_refine_prompt("", ""))
+            chunk_size = MODEL_TOKEN_LENGTH_MAPPING[
+                             self.model_name] - system_msg_length - user_msg_length - OUTPUT_TOKEN_LENGTH_BUFFER
 
+            blog_post = ""
+            while input_text:
+                utils.logging.info(f"Chunk size: {chunk_size}")
+                chunk = self.split_into_first_chunk(input_text, chunk_size)
+                user_message = self.create_refine_prompt(blog_post, chunk)
+                # TODO remove chunk from input_text, overtime it will get smaller and smaller
+                # TODO make chunking dynamic, so always full context window is used
 
+                blog_post = self._generate_answer(system_message, user_message)
+                utils.logging.info(f"Blog post: {blog_post}")
+                blog_post_length = self._count_tokens(blog_post)
+                utils.logging.info(f"Blog post token count: {blog_post_length}")
 
-        return blog_post
+                chunk_size = MODEL_TOKEN_LENGTH_MAPPING[self.model_name] - system_msg_length - user_msg_length - blog_post_length - OUTPUT_TOKEN_LENGTH_BUFFER
+                input_text = input_text.replace(chunk, "")
+                utils.logging.info(f"Remaining token count: {self._count_tokens(input_text)}")
+
+            return blog_post
