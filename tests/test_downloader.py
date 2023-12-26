efc4e8e403fe8814633307d67a8e5b1e64b28323
Jurik-001 <jurik.eineemail@gmail.com>
2023-12-26 13:06:57 +0100

    use builtin fixture

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -2,14 +2,12 @@ from essence_extractor import YouTubeDownloader
 import os
 
 
-def test_short_url():
+def test_short_url(tmp_path):
     url = "https://youtu.be/9bZkp7q19f0"
-    output_path = "videos"
-    downloader = YouTubeDownloader(output_path=output_path)
+    output_path = tmp_path / "videos"
+
+    downloader = YouTubeDownloader(output_path=str(output_path))
     video_path = downloader.download_video(url)
+
     assert video_path is not None
     assert os.path.exists(video_path)
-
-    # cleanup
-    os.remove(video_path)
-    os.rmdir(output_path)
